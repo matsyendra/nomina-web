26c5ad6711c7bb654ef546095237c88e41a6d7a1
jose antonio <campi2222@hotmail.es>
2017-09-22 02:54:42 +0200

    preparado para producción

--- public/main.js
+++ public/main.js
@@ -1,10 +1,21 @@
+    var config = {
+        apiKey: "AIzaSyDyvTK1lG1dxtidqrBZAz5fO4zGQC3YafU",
+        authDomain: "nomina-6e781.firebaseapp.com",
+        databaseURL: "https://nomina-6e781.firebaseio.com",
+        projectId: "nomina-6e781",
+        storageBucket: "nomina-6e781.appspot.com",
+        messagingSenderId: "984370076661"
+    };
+    var r = firebase.initializeApp(config);
+    var database = firebase.database();
+
     const formulario = document.getElementById("formulario");
     const tabla = document.getElementById("tabla");
 
     const et_kms = document.getElementById("et_kms");
     const _an_ti = document.getElementById("an_ti");
     const _irpf = document.getElementById("irpf");
-    const _precioKmPactado = document.getElementById("precioKmPactado");
+    const _precioKmPactado = document.getElementById("precioKmPactado_ht");
     const btn = document.getElementById("btn");
 
     if (typeof (Storage) !== "undefined") {
@@ -38,26 +49,60 @@
     const pa_be = 78.06;
     const pl_tr = 95.86;
     const k_me10 = 0.0181;
-    const k_Ma10 = 0.0323;    
+    const k_Ma10 = 0.0323;
+
+    function incrementa(raiz, variable) {
+        var databaseRef = database.ref(raiz).child(variable).child('contador');
+        databaseRef.transaction(function (searches) {            
+            return (searches || 0) + 1;
+        });
+    }
 
     function calcula() {
         tabla.style.display = 'block';
-        formulario.style.display = 'none'
+        formulario.style.display = 'none';
+        incrementa("valores_IRPF" , "Rondando el "+Math.round(_irpf.value).toString());
+        incrementa("kms_totales", et_kms.value != "" ? rangoKms(et_kms.value): "0");
+        incrementa("clics_totales", "boton_calcular");
         new Nomina(et_kms.value, _an_ti.value, _irpf.value);
     };
 
-    function Nomina(kms, an_ti, ir_pfQ) {
-        if (kms === "") kms=0;
-        if (an_ti === "") an_ti=0;
+    function recargar(){
+        incrementa("clics_totales", "boton_nueva_operacion");
+        location.reload();
+    }
 
-        let precioKmPactado = _precioKmPactado.value;
-        _precio_km.innerHTML = precioKmPactado;        
+    function rangoKms(kms){
+        if(kms<6000) return "con menos de 6000 kms";
+        if(kms<9000) return "entre 6000 y 9000 kms";
+        if(kms<10000) return "entre 9000 y 10000 kms";
+        if(kms<11000) return "10000 y pico";
+        if(kms<12000) return "11000 y pico";
+        if(kms<13000) return "12000 y pico";
+        if(kms<14000) return "13000 y pico";
+        if(kms<15000) return "14000 y pico";
+        if(kms<16000) return "15000 y pico";
+        if(kms<17000) return "16000 y pico";
+        if(kms<18000) return "17000 y pico";
+        if(kms<19000) return "18000 y pico";
+        if(kms>19000) return "con más de 19000 kms";
+    }
+
+    function Nomina(kms, an_ti, ir_pfQ) {
+        if (kms === "") kms = 0;
+        if (an_ti === "") an_ti = 0;
+        if (ir_pfQ === "") ir_pfQ = 0;
+
+        var precioKmPactado = _precioKmPactado.value != "" ? _precioKmPactado.value : 0;
+        _precio_km.innerHTML = precioKmPactado;
+        
+        incrementa("precio_km", precioKmPactado.toString().replace(".", ","));
 
         if (typeof (Storage) !== "undefined") {
             localStorage.setItem("irpf", ir_pfQ);
             localStorage.setItem("preciokm", precioKmPactado);
         }
-        let co_coQ = 4.7,
+        var co_coQ = 4.7,
             de_seQ = 1.55,
             fo_prQ = 0.1,
             re_exQ = 4.7;
@@ -65,39 +110,39 @@
         kms_re.innerHTML = kms;
         _ta_salarial.innerHTML = tabla_salarial;
         _sa_ba.innerHTML = sa_ba;
-        let kilometrosFijos = kms > 10000 ? k_me10 * 10000 + ((kms - 10000) * (k_Ma10)) : kms * k_me10;
+        var kilometrosFijos = kms > 10000 ? k_me10 * 10000 + ((kms - 10000) * (k_Ma10)) : kms * k_me10;
 
-        let ho_ex = kilometrosFijos * (0.5);
+        var ho_ex = kilometrosFijos * (0.5);
         _ho_ex.innerHTML = ho_ex.toFixed(2);
         _pl_as.innerHTML = pl_as;
-        let pl_no = kilometrosFijos * 0.1;
+        var pl_no = kilometrosFijos * 0.1;
         _pl_no.innerHTML = pl_no.toFixed(2);
-        let ho_pr = kilometrosFijos * 0.4;
+        var ho_pr = kilometrosFijos * 0.4;
         _ho_pr.innerHTML = ho_pr.toFixed(2);
         _pl_tr.innerHTML = pl_tr;
         _pa_be.innerHTML = pa_be;
 
-        let di_et = (kms * precioKmPactado) - kilometrosFijos;
+        var di_et = (kms * precioKmPactado) - kilometrosFijos;
         _di_et.innerHTML = di_et.toFixed(2);
-        let to_dev = ho_ex + pl_no + ho_pr + di_et + sa_ba + pl_tr + pa_be + pl_as;
+        var to_dev = ho_ex + pl_no + ho_pr + di_et + sa_ba + pl_tr + pa_be + pl_as;
         _to_dev.innerHTML = to_dev.toFixed(2);
-        let co_co = (pl_tr + sa_ba + ho_ex + pl_as + pa_be + (sa_ba / 6)) * co_coQ / 100;
-        let de_se = (((pl_tr + sa_ba + ho_ex + pl_as + pa_be + (sa_ba / 6)) + ho_ex) * de_seQ) / 100;
-        let fo_pr = (((pl_tr + sa_ba + ho_ex + pl_as + pa_be + (sa_ba / 6)) + ho_ex) * fo_prQ) / 100;
-        let re_ex = ho_ex * re_exQ / 100;
+        var co_co = (pl_tr + sa_ba + ho_ex + pl_as + pa_be + (sa_ba / 6)) * co_coQ / 100;
+        var de_se = (((pl_tr + sa_ba + ho_ex + pl_as + pa_be + (sa_ba / 6)) + ho_ex) * de_seQ) / 100;
+        var fo_pr = (((pl_tr + sa_ba + ho_ex + pl_as + pa_be + (sa_ba / 6)) + ho_ex) * fo_prQ) / 100;
+        var re_ex = ho_ex * re_exQ / 100;
 
         ir_pf = (to_dev - di_et) * ir_pfQ / 100;
         to_ap = co_co + de_se + fo_pr + re_ex;
         _to_aportaciones.innerHTML = to_ap.toFixed(2);
-        let msg = "IRPF (".concat(ir_pfQ, "%)");
+        var msg = "IRPF (".concat(ir_pfQ, "%)");
         _irpf_p.innerHTML = msg;
         _irpf_r.innerHTML = ir_pf.toFixed(2);
-        let anticipos = parseFloat(an_ti);
+        var anticipos = parseFloat(an_ti);
         _anticipos.innerHTML = anticipos
-        let to_ded = to_ap + ir_pf + anticipos;
+        var to_ded = to_ap + ir_pf + anticipos;
         _to_deducir.innerHTML = parseFloat(to_ded).toFixed(2);
         to_a_percibir = to_dev - to_ded;
         _li_percibir.innerHTML = parseFloat(to_a_percibir).toFixed(2);
-        let TT_RE = to_a_percibir + anticipos;
+        var TT_RE = to_a_percibir + anticipos;
         _to_retribuido.innerHTML = parseFloat(TT_RE).toFixed(2);
     }
