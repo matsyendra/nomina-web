c055d3794caed2dde580d9783654ce9ed4f4e495
jose antonio <campi2222@hotmail.es>
2017-10-12 09:31:07 +0200

    desecho el entuerto, y a la espera de un toolbar

--- desarrollo/main.js
+++ desarrollo/main.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
     var config = {
         apiKey: "AIzaSyDyvTK1lG1dxtidqrBZAz5fO4zGQC3YafU",
         authDomain: "nomina-6e781.firebaseapp.com",
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,3 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
-
 self.addEventListener('install', function (event) {
     console.log('Service Worker installing.');
 });
