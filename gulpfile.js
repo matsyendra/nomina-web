a0f04c5a5420a043189388e5464631d3a04235fb
jose antonio <campi2222@hotmail.es>
2017-10-14 12:53:15 +0200

    sw con sw-precache y gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Include Gulp & Tools We'll Use
+var gulp = require('gulp');
+
+gulp.task('generate-service-worker', function(callback) {
+    var path = require('path');
+    var swPrecache = require('sw-precache');
+    var rootDir = 'gulp';
+  
+    swPrecache.write(path.join(rootDir, 'sw.js'), {
+      staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+      stripPrefix: rootDir
+    }, callback);
+  });gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'public';
+
+  swPrecache.write(path.join(rootDir, 'sw.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+    stripPrefix: rootDir
+  }, callback);
+});
